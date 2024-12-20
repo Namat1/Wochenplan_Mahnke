ff1850fbb17f3664341321fd917f95ffb49dff36
Namat1 <mcarstensen79@gmail.com>
2024-12-21 00:52:10 +0100

    Update mahnke.py

--- mahnke.py
+++ mahnke.py
@@ -29,9 +29,9 @@ def extract_work_data(df):
 
     row_index = start_index
     while row_index <= end_index:
-        # Nachname in Großbuchstaben und Vorname unverändert
-        lastname = str(df.iloc[row_index, 1]).strip().upper()  # Spalte B
-        firstname = str(df.iloc[row_index, 2]).strip()  # Spalte C
+        # Nachname mit nur den ersten Buchstaben groß und Vorname auch mit nur den ersten Buchstaben groß
+        lastname = str(df.iloc[row_index, 1]).strip().title()  # Spalte B
+        firstname = str(df.iloc[row_index, 2]).strip().title()  # Spalte C
 
         # Überspringe Zeilen, bei denen Nachname oder Vorname fehlt
         if not lastname or not firstname:
